886e5a76bde49738123c37e0093da9f4b1e099af
codewith-rafi <codewithrafi2002@gmail.com>
2025-10-07 22:04:09 +0800

    riverpod annotation and generator

--- lib/providers/product_providers.dart
+++ lib/providers/product_providers.dart
@@ -1,5 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:riverpod_tutorial/models/product.dart';
+part 'product_providers.g.dart';
 
 const List<Product> allProducts = [
   Product(id: '1', title: 'Red Backpack', price: 9.99, image: 'assets/products/backpack.png'),
@@ -11,10 +13,22 @@ const List<Product> allProducts = [
   Product(id: '7', title: 'Blue Suitcase', price: 69.99, image: 'assets/products/suitcase.png'),
 ];
 
-final productsProvider = Provider((ref) {
-  return allProducts;
-});
+// final productsProvider = Provider((ref) {
+//   return allProducts;
+// });
 
-final reducedProductsProvider = Provider((ref) {
+// final reducedProductsProvider = Provider((ref) {
+//   return allProducts.where((p) => p.price < 50).toList();
+// });
+
+// generated providers
+
+@riverpod
+List<Product> products(ref) {
+  return allProducts;
+}
+
+@riverpod
+List<Product> reducedProducts(ref) {
   return allProducts.where((p) => p.price < 50).toList();
-});
+}
