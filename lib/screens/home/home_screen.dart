22d7f90708fea0125f664c2ce59a50360987460d
codewith-rafi <codewithrafi2002@gmail.com>
2025-10-07 18:20:51 +0800

    add riverpod to cart and home screen

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_tutorial/providers/product_providers.dart';
 import 'package:riverpod_tutorial/shared/cart_icon.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends ConsumerWidget {
   const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final allProducts = ref.watch(productsProvider);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Garage Sale Products'),
@@ -14,7 +18,7 @@ class HomeScreen extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.all(20),
         child: GridView.builder(
-          itemCount: 8,
+          itemCount: allProducts.length,
           gridDelegate:const SliverGridDelegateWithFixedCrossAxisCount(
             crossAxisCount: 2,
             mainAxisSpacing: 20,
@@ -25,6 +29,13 @@ class HomeScreen extends StatelessWidget {
             return Container(
               padding: const EdgeInsets.all(20),
               color: Colors.blueGrey.withValues(alpha: 0.05),
+              child: Column(
+                children: [
+                  Image.asset(allProducts[index].image, height: 60, width: 60,),
+                  Text(allProducts[index].title, style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),),
+                  Text("\$${allProducts[index].price}")
+                ],
+              ),
             );
           },
         ),
